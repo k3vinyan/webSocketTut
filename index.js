35ace812f72e47842cfed3feee4c03dee1216383
Kevin Yan <k3vinyan@gmail.com>
2017-08-10 17:49:05 -0700

    update package.json and ad mongo modules

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var socket = require('socket.io');
+// var MongoClient = require('mongodb').MongoClient;
+//
+// var url = "mongodb://localhost:27017/mydb";
+//
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   db.createCollection("tbas", function(err, res){
+//     if(err) throw err;
+//   })
+//   console.log("Database created!");
+//   db.close();
+// });
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({'extended': 'true'}));
