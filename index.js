96542c1a6baf85beb089e11882fa9c1efb400338
Kevin Yan <k3vinyan@gmail.com>
2017-08-10 18:05:27 -0700

    fix typo

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var socket = require('socket.io');
 //   db.close();
 // });
 
-var port = provess.env.PORT || 8080;
+var port = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
